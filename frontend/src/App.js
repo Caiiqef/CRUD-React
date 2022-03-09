36e3d788ea971d2ac20c61f5a7383725d671c024
Caique Fernandes <caiiqe.fernandes@hotmail.com>
2022-03-09 17:17:58 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+ 
+function App() {
+  return (
+    <Router>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <Router>
+            <Route exact path="/">
+              <ProductList />
+            </Route>
+            <Route path="/add">
+              <AddProduct />
+            </Route>
+            <Route path="/edit/:id">
+              <EditProduct />
+            </Route>
+          </Router>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
+}
+ 
+export default App;
